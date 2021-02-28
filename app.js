656026c1f02ffb63ea46b8b0d67bbf19a5de97ed
Alex White <pixeljuggle@gmail.com>
2021-02-28 17:10:33 +0000

    Minor style adjustments

--- app.js
+++ app.js
@@ -14,26 +14,36 @@ function toggleTheme() {
 
 // Drum Kit
 
-
-window.addEventListener('keydown', function(e){
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if(!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-  key.classList.add('playing');
-
-});
-
-function removeTransition(e){
-  if(e.propertyName !== 'font-size') return;
-  this.classList.remove('playing');
-  
+function removeTransition(e) {
+  if (e.propertyName !== 'color') return;
+  e.target.classList.remove('playing');
 }
 
-const keys = document.querySelectorAll('.key');
+function playSound(e) {
+  console.log(e)
+  let keyCode;
+  if (e.type === 'click') {
+    keyCode = Number(e.target.dataset.key);
+  } else {
+    keyCode = e.keyCode;
+  }
+
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
+window.addEventListener('keydown', playSound);
+keys.forEach(key => key.addEventListener('click', playSound));
 
 // Initial run for theme
 (function () {

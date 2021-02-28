79c27c2afcb0799e4cefe201f0314f52c0c61dc2
Alex White <pixeljuggle@gmail.com>
2021-02-28 16:17:47 +0000

    Adding favicon and basic drum functionality

--- app.js
+++ app.js
@@ -12,6 +12,30 @@ function toggleTheme() {
   }
 }
 
+// Drum Kit
+
+
+window.addEventListener('keydown', function(e){
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+
+});
+
+function removeTransition(e){
+  if(e.propertyName !== 'font-size') return;
+  this.classList.remove('playing');
+  
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+// Initial run for theme
 (function () {
   if (localStorage.getItem("theme") === "theme-dark") {
     setTheme("theme-dark");
